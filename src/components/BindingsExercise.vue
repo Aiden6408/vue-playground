bae9244d6719b9dac4dd3c171cfc8daeac415f41
Aiden Eskatakii <aidenjameseskatakii@gmail.com>
2022-03-08 09:12:22 -0700

    done

--- src/components/BindingsExercise.vue
+++ src/components/BindingsExercise.vue
@@ -11,7 +11,7 @@
             Check out what happens below. Now go change the value of message in
             our components script area and see what happens.
           </p>
-          <p>{{}}</p>
+          <p>{{ state.message }}</p>
         </div>
         <div class="border p-1">
           <h5>Two Way Data Binding</h5>
@@ -22,9 +22,9 @@
             value changes.
           </p>
           <!-- add a v-model to this input that will reference "myName" within the state object-->
-          <input />
+          <input v-model="state.myname" />
           <!-- reference "myName" from the state -->
-          <p>{{}}</p>
+          <p>{{ state.myname }}</p>
         </div>
         <div class="border p-1">
           <h5>Class Binding</h5>
@@ -53,7 +53,8 @@ export default {
   setup() {
     const state = reactive({
       //create a property called myName and set its value to "type your name here"
-      message: "Super secret message here!",
+      message: "Oh no,This is too awesome!",
+      myname: "aiden",
       //change isActive to true
       isActive: false,
     });
